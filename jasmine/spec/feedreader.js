9469bd95cc748f784020c95c77937f2afc60e20b
Shannon <13708207+smckinney1@users.noreply.github.com>
2017-12-26 20:10:46 -0500

    New Feed Selection tests not working, saving code

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -22,21 +22,21 @@ $(function() {
         // Loops through each feed and ensures it contains URLS
         // that are both defined and not empty.
         // "toBeTruthy" accomplishes both tasks at once.
-         it('contain valid URLs', function() {
+        it('contain valid URLs', function() {
             allFeeds.forEach(function(feed) {
                 var url = feed.url;
                 expect(url).toBeTruthy();
             });
-         });
+        });
 
         // Loops through each feed and ensures it contains names
         // that are both defined and not empty.
-         it('contain valid names', function() {
+        it('contain valid names', function() {
             allFeeds.forEach(function(feed) {
                 var name = feed.name;
                 expect(name).toBeTruthy();
             });
-         });
+        });
     });
 
     describe('The Menu', function() {
@@ -45,32 +45,32 @@ $(function() {
             menuIcon = $('.menu-icon-link');
 
         // Menu should be hidden by default
-         it('is hidden by default', function() {
+        it('is hidden by default', function() {
             expect(body.hasClass('menu-hidden')).toBe(true);
-         });
+        });
 
          // When menu link is clicked, visibility of menu toggles
-          it('changes visibility when menu is clicked', function() {
+        it('changes visibility when menu is clicked', function() {
             menuIcon.click();
             expect(body.hasClass('menu-hidden')).toBe(false);
             menuIcon.click();
             expect(body.hasClass('menu-hidden')).toBe(true);
-          });
+        });
 
     });
 
     describe('Initial Entries', function() {
-         var feedContainer = $('.feed');
+        var feedContainer = $('.feed');
 
-         // loadFeed is asynchronous, so must use "done" operation
-         beforeEach(function(done) {
+        // loadFeed is asynchronous, so must use "done" operation
+        beforeEach(function(done) {
             loadFeed(0, done);
-         });
+        });
 
-         // Ensure there are children of feedContainer (i.e., feed items)
-         it('has at least one .entry element within the feed container', function() {
+        // Ensure there are children of feedContainer (i.e., feed items)
+        it('has at least one .entry element within the feed container', function() {
             expect(feedContainer.children().length).not.toBe(0);
-         });
+        });
 
     });
 
@@ -79,6 +79,24 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        var indexToTest = 1,
+            initialFeedHtml = $('.feed').html();
+
+        beforeEach(function(done) {
+            var newFeedSelection = $('.feed-list li')[indexToTest];
+            newFeedSelection.click();
+            done();
+        });
+
+        it('updates the feed items', function() {
+            var newFeedHtml = $('.feed').html();
+            expect(initialFeedHtml).not.toBe(newFeedHtml);
+        });
+
+        it('has correct HTML', function() {
+            var newFeedHtml = $('.feed').html();
+            expect(newFeedHtml).toContain('css-tricks');
+        });
 
      });
 
